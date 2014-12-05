f280c770ab1b92d791262de7ecb02efa94a527ca
Random Xyzzy <rndmxyzzy@gmail.com>
2014-12-05 17:18:54 +0900

    Protection against portal names with newline or tab

--- export-portals-list.user.js
+++ export-portals-list.user.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-export-portals-list@randomizax
 // @name           IITC plugin: export list of portals
 // @category       Info
-// @version        0.2.0.20141205.094341
+// @version        0.2.0.20141205.171715
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://rawgit.com/randomizax/export-portals-list/latest/export-portals-list.meta.js
 // @downloadURL    https://rawgit.com/randomizax/export-portals-list/latest/export-portals-list.user.js
-// @description    [jonatkins-2014-12-05-094341] Display exportable list of portals as TSV(CSV).
+// @description    [jonatkins-2014-12-05-171715] Display exportable list of portals as TSV(CSV).
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
@@ -251,6 +251,7 @@ function wrapper(plugin_info) {
           cell.textContent = value;
         }
 
+        value = value.replace(/[\n\t]/g, "");
         tsvColumns.push(field.tsv ? field.tsv(portal, value) : value);
       });
 
