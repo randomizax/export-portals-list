1b8f39bb20d96049dbd12e2b5ef82a3cc762b1df
Random Xyzzy <rndmxyzzy@gmail.com>
2014-12-12 00:53:34 +0900

    Update meta

--- export-portals-list.meta.js
+++ export-portals-list.meta.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-export-portals-list@randomizax
 // @name           IITC plugin: export list of portals
 // @category       Info
-// @version        0.3.0.20141208.012852
+// @version        0.3.1.20141212.004952
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://rawgit.com/randomizax/export-portals-list/latest/export-portals-list.meta.js
 // @downloadURL    https://rawgit.com/randomizax/export-portals-list/latest/export-portals-list.user.js
-// @description    [jonatkins-2014-12-08-012852] Display exportable list of portals as TSV(CSV).
+// @description    [jonatkins-2014-12-12-004952] Display exportable list of portals as TSV(CSV).
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
