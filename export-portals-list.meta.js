ad7bef9f2e7d1ec66bc4f3cc052c79c83613dfcd
Random Xyzzy <rndmxyzzy@gmail.com>
2025-04-20 14:29:13 +0900

    fix @include

--- export-portals-list.meta.js
+++ export-portals-list.meta.js
@@ -2,14 +2,12 @@
 // @id             iitc-plugin-export-portals-list@randomizax
 // @name           IITC plugin: export list of portals
 // @category       Info
-// @version        1.0.1.20190314.160024
-// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @version        2.0.0.20250420.52309
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
 // @updateURL      https://randomizax.github.io/export-portals-list/export-portals-list.meta.js
 // @downloadURL    https://randomizax.github.io/export-portals-list/export-portals-list.user.js
-// @description    [randomizax-2019-03-14-160024] Display exportable list of portals as TSV(CSV).
-// @include        https://*.ingress.com/intel*
-// @include        http://*.ingress.com/intel*
-// @match          https://*.ingress.com/intel*
-// @match          http://*.ingress.com/intel*
+// @description    [randomizax-2025-04-20-052309] Display exportable list of portals as TSV(CSV).
+// @include        https://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
--- export-portals-list.user.js
+++ export-portals-list.user.js
@@ -2,15 +2,13 @@
 // @id             iitc-plugin-export-portals-list@randomizax
 // @name           IITC plugin: export list of portals
 // @category       Info
-// @version        1.0.1.20190314.160024
-// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @version        2.0.0.20250420.52309
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
 // @updateURL      https://randomizax.github.io/export-portals-list/export-portals-list.meta.js
 // @downloadURL    https://randomizax.github.io/export-portals-list/export-portals-list.user.js
-// @description    [randomizax-2019-03-14-160024] Display exportable list of portals as TSV(CSV).
-// @include        https://*.ingress.com/intel*
-// @include        http://*.ingress.com/intel*
-// @match          https://*.ingress.com/intel*
-// @match          http://*.ingress.com/intel*
+// @description    [randomizax-2025-04-20-052309] Display exportable list of portals as TSV(CSV).
+// @include        https://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
 
@@ -22,7 +20,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 // plugin_info.buildName = 'randomizax';
-// plugin_info.dateTimeVersion = '20190314.160024';
+// plugin_info.dateTimeVersion = '20250420.52309';
 // plugin_info.pluginId = 'export-portals-list';
 //END PLUGIN AUTHORS NOTE
 
